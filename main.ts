f636abd04c0e21152c41ac4193434ac3459321b8
Loopydos <b29961846@gmail.com>
2025-06-03 12:10:00 +0200

    Actualització main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,20 @@
+let LEDUP = 0
+pins.digitalWritePin(DigitalPin.P0, 0)
 basic.forever(function () {
-    pins.digitalWritePin(DigitalPin.P0, 1)
+    if (input.soundLevel() > 140) {
+        if (LEDUP == 1) {
+            pins.digitalWritePin(DigitalPin.P0, 0)
+            basic.pause(500)
+            LEDUP = 0
+        }
+    }
+})
+basic.forever(function () {
+    if (LEDUP == 0) {
+        if (input.soundLevel() > 140) {
+            pins.digitalWritePin(DigitalPin.P0, 1)
+            basic.pause(500)
+            LEDUP = 1
+        }
+    }
 })
