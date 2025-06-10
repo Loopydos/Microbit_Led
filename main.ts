9587323d2a0cc4085f460ac3950ab72cc7ea8769
Loopydos <b29961846@gmail.com>
2025-06-10 11:49:58 +0200

    Actualització main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
 let LEDUP = 0
 pins.digitalWritePin(DigitalPin.P0, 0)
 basic.forever(function () {
-    if (input.soundLevel() > 140) {
+    if (input.soundLevel() > 130) {
         if (LEDUP == 1) {
             pins.digitalWritePin(DigitalPin.P0, 0)
             basic.pause(500)
@@ -11,7 +11,7 @@ basic.forever(function () {
 })
 basic.forever(function () {
     if (LEDUP == 0) {
-        if (input.soundLevel() > 140) {
+        if (input.soundLevel() > 130) {
             pins.digitalWritePin(DigitalPin.P0, 1)
             basic.pause(500)
             LEDUP = 1
